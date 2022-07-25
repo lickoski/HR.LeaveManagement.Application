142824d092ca9f1d2c236ce8c2574b69da745335
Tiago Lickoski <tiago.lickoski@squadra.com.br>
2022-07-25 08:50:59 -0300

    First commit

---
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,15 @@
+﻿using HR.LeaveManagement.Application.Profiles;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(typeof(MappingProfile));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using System;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+
+        public DateTime DateRequested { get; set; }
+
+        public bool? Approved { get; set; }
+    }
+}
